560e1d8661594bef8ed22617093b7a628344af1f
Bruno Brustoloni e Oliveira <brustoloni@ciandt.com>
2019-01-08 19:10:26 -0200

    bla

---
+++ app/src/main/java/bbrustol/com/moodelizertest/ssss.java
@@ -0,0 +1,8 @@
+package bbrustol.com.moodelizertest;
+
+//public class ssss {
+//    private final static int MAX_VOLUME = 100
+//    final float volume = (float) (1 - (Math.log(MAX_VOLUME - soundVolume) / Math.log(MAX_VOLUME)));
+//    mediaPlayer.setVolume(volume, volume);
+//
+//}
